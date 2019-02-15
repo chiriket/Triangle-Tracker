c4766424010e80fb57096a7ad329bcb82c76ea44
Shirley Keter <shirleyketer@gmail.com>
2019-02-15 12:53:36 +0300

    add script prompt

---
+++ script.js
@@ -0,0 +1,4 @@
+
+var base = parseInt(prompt("Enter base length of the triangle"));
+var height = parseInt(prompt("Enter height length of the triangle"));
+var hypotenus = parseInt(prompt("Enter hypotenus length of the triangle"));
